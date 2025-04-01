394d3789ccc63e2d3d16de09798cd40b887d7485
Spencer Hales <spencerhales@macbookpro.lan>
2025-03-31 23:22:27 -0600

    Added necessary information for deliverable 8

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   metrics: {
     source: 'jwt-pizza-service-dev',
-    url: 'https://influx-prod-13-prod-us-east-0.grafana.net/api/v1/push/influx/write',
-    apiKey: '1212707:glc_eyJvIjoiMTM4ODQyNyIsIm4iOiJzdGFjay0xMjEyNzA3LWludGVncmF0aW9uLWp3dC1waXp6YS1tZXRyaWNzIiwiayI6Ims1NFYwazU3dmhEaTQ2WWdFMTZOMk52SiIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19'
+    url: process.env.METRICS_URL || 'https://influx-prod-13-prod-us-east-0.grafana.net/api/v1/push/influx/write',
+    apiKey: process.env.METRICS_API_KEY
   },
 };
